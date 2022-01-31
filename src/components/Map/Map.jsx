fe4508bb0abbf38cef854d1347ee53d336ba5b94
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-01-31 18:52:02 +0300

    components/Map&List&PlaceDetails updated

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
+
 
 const Map = () => {
-  return <div>Map</div>;
+
+
+  const isMobile = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+
+  const coordinates={lang:0,lat:0}
+
+  return (
+    <div className={classes.mapContainer}>
+    <GoogleMapReact
+      bootstrapURLKeys={{ key:''}}
+      defaultCenter={coordinates}
+      center={coordinates}
+      defaultZoom={14}
+      margin={[50, 50, 50, 50]}
+      options={""}
+      onChange={""}
+      onChildClick={""}
+    >
+     
+    </GoogleMapReact>
+  </div>
+  );
 };
 
 export default Map;
