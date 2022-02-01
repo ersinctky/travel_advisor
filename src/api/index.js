914570bdd0b0205049807cfd65707ebf11c6999f
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-01 12:14:26 +0300

    src/api/index.js types props added

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,28 @@
 import axios from "axios";
 
-const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng";
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (types, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        latitude: "12.91285",
-        longitude: "100.87808",
-        limit: "30",
-        currency: "USD",
-        distance: "2",
-        open_now: "false",
-        lunit: "km",
-        lang: "en_US",
-      },
-      headers: {
-        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${types}/list-by-latlng`,
+      {
+        params: {
+          latitude: "12.91285",
+          longitude: "100.87808",
+          limit: "30",
+          currency: "USD",
+          distance: "2",
+          open_now: "false",
+          lunit: "km",
+          lang: "en_US",
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        },
+      }
+    );
     return data;
   } catch (error) {
     console.log(error);
