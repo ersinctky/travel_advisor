38398daca35727a856d121483706d8218740e734
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-01 14:03:51 +0300

    src/components/Map mapStyles added & options updated

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,6 +4,8 @@ import {  Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
+import mapStyles from '../../mapStyles';
+
 import useStyles from './styles.js';
 
 
@@ -22,7 +24,7 @@ const Map = ({coords, places, setCoords,setChildClicked, setBounds,weatherData})
       center={coords}
       defaultZoom={14}
       margin={[50, 50, 50, 50]}
-      options={""}
+      options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
       onChange={(e) => {
         setCoords({ lat: e.center.lat, lng: e.center.lng });
         setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
